6c46c9203df0ade3d33df330d6b36567585b8f0e
ahmad-saleh <ahmad.saleh@milanlaser.com>
2022-04-27 15:28:36 -0500

    Gallery Page

---
+++ src/pages/gallery.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+function GalleryPage() {
+  return (
+    <Layout>
+      <Seo title="Books" />
+      <h1>Gallery Page</h1>
+    </Layout>
+  )
+}
+
+export default GalleryPage
