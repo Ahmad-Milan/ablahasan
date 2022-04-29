3780a8682abfb9f24955b1441d6bc8433976a410
ahmad-saleh <ahmad.saleh@milanlaser.com>
2022-04-29 13:46:47 -0500

    media page

--- src/pages/media.js
+++ src/pages/media.js
@@ -1,15 +1,24 @@
 import * as React from "react"
-import { Link } from "gatsby"
-
+import YouTubeSection from '../components/media/YouTubeSection'
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { Container, Row, Col } from "react-bootstrap"
+
 
 const MediaPage = () => {
   return (
     <Layout>
       <Seo title="Media" />
-      <h1>Media page</h1>
-      <Link to="/">Go back to the homepage</Link>
+      <section className="hero media-hero mb-5 d-flex align-items-end">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="fw-normal display-4 text-gray-blue mb-5 pb-md-2">Media</h1>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <YouTubeSection />
     </Layout>
   )
 }
