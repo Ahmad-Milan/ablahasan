6589d51ab04ea9e044b3ada6dd8206e84f0e7079
ahmad-saleh <ahmad.saleh@milanlaser.com>
2022-05-03 15:47:06 -0500

    gallery page created

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import Gallery from '../components/gallery/Gallery'
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { Container, Row, Col } from "react-bootstrap"
+// import Carousel from 'react-gallery-carousel';
+// import 'react-gallery-carousel/dist/index.css';
 
 function GalleryPage() {
   return (
     <Layout>
       <Seo title="Books" />
-      <h1>Gallery Page</h1>
+      <section className="hero gallery-hero mb-5 d-flex align-items-end">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="fw-normal display-4 text-gray-blue mb-5 pb-md-2">Gallery</h1>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <Gallery />
     </Layout>
   )
 }
