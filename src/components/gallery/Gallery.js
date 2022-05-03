4520dced85db21fa922f1b4c68d8feba7c2a74b8
ahmad-saleh <ahmad.saleh@milanlaser.com>
2022-05-03 15:46:32 -0500

    Gallery comp added

---
+++ src/components/gallery/Gallery.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import { graphql, useStaticQuery } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
+function Gallery() {
+
+  const localImagesData = useStaticQuery(graphql`
+    query localImages {
+      allFile(filter: {relativeDirectory: {eq: "gallery"}})  {
+        edges {
+          node {
+            base
+            childImageSharp {
+              gatsbyImageData(
+                placeholder: BLURRED
+                webpOptions: {quality: 90}
+                quality: 90
+              )
+            }
+            relativePath
+          }
+        }
+      }
+    }
+  `).allFile.edges
+
+console.log(localImagesData)
+  return (
+    <Container id="gallery">
+      {
+        localImagesData.length && 
+        <Row>
+          {
+            localImagesData.map((photo, x) => (
+            <Col key={x} md={6} lg={4}>
+              <figure className="overflow-hidden">
+                <GatsbyImage image={getImage(photo.node)} alt="photo gallery" />
+              </figure>
+            </Col>
+            ))
+          }
+        </Row>
+      }
+    </Container>
+  )
+}
+
+export default Gallery
+
