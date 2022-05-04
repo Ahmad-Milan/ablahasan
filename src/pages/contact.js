cbdb03a9f88c187442d1824c8ec91e3dce8d7434
ahmad-saleh <ahmad.saleh@milanlaser.com>
2022-05-04 13:39:19 -0500

    contact page added

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Container, Row, Col } from "react-bootstrap"
+import ContactForm from '../components/contact/Form'
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -7,9 +8,18 @@ import Seo from "../components/seo"
 const ContactPage = () => (
   <Layout>
     <Seo title="Contact" />
-    <h1>Hi from the Contact page</h1>
+    <section className="hero contact-hero mb-5 d-flex align-items-end">
+      <Container>
+        <Row>
+          <Col>
+            <h1 className="fw-normal display-4 text-gray-blue">CONTACT</h1>
+            <h2 className="fw-normal mb-5 pb-md-2 text-gray-blue">Porfessor Abla Hasan</h2>
+          </Col>
+        </Row>
+      </Container>
+    </section>
 
-    <Link to="/">Go back to the homepage</Link>
+    <ContactForm />
   </Layout>
 )
 
