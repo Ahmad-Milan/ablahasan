aeb9ec0594aeb6a85b9bbb96cb9b184de611a298
ahmad-saleh <ahmad.saleh@milanlaser.com>
2022-05-03 20:45:47 -0500

    adding modal to gallery

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -2,9 +2,13 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { graphql, useStaticQuery } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import GalleryModal from './GalleryModal'
+import { AiOutlineZoomIn } from 'react-icons/ai'
 
 function Gallery() {
 
+  const [modalShow, setModalShow] = React.useState(false)
+  const [activeIndex, setActiveIndex] = React.useState(0)
   const localImagesData = useStaticQuery(graphql`
     query localImages {
       allFile(filter: {relativeDirectory: {eq: "gallery"}})  {
@@ -25,23 +29,31 @@ function Gallery() {
     }
   `).allFile.edges
 
+  const handleDisplayImage = (e, index) => {
+    setModalShow(true)
+    setActiveIndex(index)
+  }
+
   return (
-    <Container id="gallery">
-      {
-        localImagesData.length && 
+    <section id="gallery">
+      <Container>
         <Row>
           {
             localImagesData.map((photo, x) => (
             <Col key={x} md={6} lg={4}>
-              <figure className="overflow-hidden">
+              <figure className="overflow-hidden position-relative" onClick={e => handleDisplayImage(e, x)}>
                 <GatsbyImage image={getImage(photo.node)} alt="photo gallery" />
+                <span className="position-absolute w-100 h-100 d-flex zoom">
+                  <AiOutlineZoomIn className="m-auto display-3" />
+                </span>
               </figure>
             </Col>
             ))
           }
         </Row>
-      }
-    </Container>
+      </Container>
+      <GalleryModal show={modalShow} onHide={() => setModalShow(false)} {...{localImagesData, activeIndex}} />
+    </section>
   )
 }
 
