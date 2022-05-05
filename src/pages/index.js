25942e85173ffdde8ba7bf60a59bcbb37a886c32
ahmad-saleh <ahmad.saleh@milanlaser.com>
2022-05-05 10:45:09 -0500

    adding h3

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,8 @@ const IndexPage = () => (
         <Col lg={6}>
           <div className="mb-4">
             <h1 className="display-3 mb-4">Dr. Abla Hasan</h1>
-            <h2 className="display-6 text">Associate Prof of practice of Arabic language and culture </h2>
+            <h2 className="display-6 mb-4 text">Associate Prof of practice of Arabic language and culture </h2>
+            <h3 className="h2 fw-light text">- Nebraska University</h3>
           </div>
         </Col>
         <Col lg={6}>
