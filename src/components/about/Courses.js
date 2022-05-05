b51ab71b6841b10b0f5967fa88fbf2beb7484b3c
ahmad-saleh <ahmad.saleh@milanlaser.com>
2022-05-05 11:13:04 -0500

    minor change

--- src/components/about/Courses.js
+++ src/components/about/Courses.js
@@ -11,7 +11,7 @@ function Courses() {
   }
   return (
     <Container className="pt-5 mt-md-3 courses">
-      <h3 className="mb-0 display-6 text-gray-blue text-uppercase">Courses I have taught at UNL:</h3>
+      <h3 className="mb-0 display-6 text-gray-blue text-uppercase">Courses I have taught at UNL</h3>
       <Underline />
       <Row 
         className="overflow-hidden"
@@ -37,14 +37,14 @@ function Courses() {
             <ListGroup.Item><GoPrimitiveDot /> ARAB 288 Exploring love, sexuality and femininity in the History of the Arabic culture.</ListGroup.Item>
             <ListGroup.Item><GoPrimitiveDot /> ARAB 306 Women the in Qur’an</ListGroup.Item>
             <ListGroup.Item><GoPrimitiveDot /> RELG/ENG 342 The Qur’an.</ListGroup.Item>
-            <ListGroup.Item><GoPrimitiveDot /> ARAB 306 “Women in the Qur’an” as the first course taught at UNL through Nebraska’s Global Virtual Project. This course was offered by spring 2018 jointly with Nizwa University in Oman. The project is supported by the Aspen Institute’s Stevens Initiative and the U.S. Department of State’s Bureau of Educational and Cultural Affairs. A link to the course https://www.youtube.com/watch?v=znRATczGelk</ListGroup.Item>
+            <ListGroup.Item><GoPrimitiveDot /> ARAB 306 “Women in the Qur’an” as the first course taught at UNL through Nebraska’s Global Virtual Project. This course was offered by spring 2018 jointly with Nizwa University in Oman. The project is supported by the Aspen Institute’s Stevens Initiative and the U.S. Department of State’s Bureau of Educational and Cultural Affairs. <a href="https://www.youtube.com/watch?v=znRATczGelk" target="_blank" rel="noreferrer">A link to the course</a> </ListGroup.Item>
             <ListGroup.Item><GoPrimitiveDot /> ARAB 306 “ Women in the Qur’an” as the first UNL Digital Islamic shared curriculum (DISC) course offered by UNL through DISC and received by Rutgers University, Spring 2018 and Spring 2019.This course was also received by Penn State University by Spring 2020.</ListGroup.Item>
             <ListGroup.Item><GoPrimitiveDot /> WMNS 250 Sex and Gender around the Globe. Both in-Persona and online.</ListGroup.Item>
             <ListGroup.Item><GoPrimitiveDot /> Honors 102 H “The science of you”.</ListGroup.Item>
             <ListGroup.Item><GoPrimitiveDot /> ARAB 391 Global Islam: Current Debates.</ListGroup.Item>
             <ListGroup.Item><GoPrimitiveDot /> ARAB 396 Independent study: Stories in the Qur’an.</ListGroup.Item>
             <ListGroup.Item><GoPrimitiveDot /> ARAB 396 Independent study: Themes in the Qur’an.</ListGroup.Item>
-            <ListGroup.Item><GoPrimitiveDot /> Honors MODL 189 H “Arab intellectuals on love and gender” ACE 5. A link to a video https://mediahub.unl.edu/media/12744</ListGroup.Item>
+            <ListGroup.Item><GoPrimitiveDot /> Honors MODL 189 H “Arab intellectuals on love and gender” ACE 5. <a href="https://mediahub.unl.edu/media/12744" target="_blank" rel="noreferrer">A link to a video</a> </ListGroup.Item>
           </ListGroup>
         </Col>
       </Row>
