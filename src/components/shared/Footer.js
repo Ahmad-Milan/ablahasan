b88c162273a26c8a4d1d0c3864a3c91cee23977c
ahmad-saleh <ahmad.saleh@milanlaser.com>
2022-05-05 19:27:24 -0500

    footer created

---
+++ src/components/shared/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import DownloadCV from './DownloadCV'
+
+function Footer() {
+  return (
+    <footer className="mt-4 py-5">
+      <Container>
+        <Row>
+          <Col className="mb-4 text-center" md={6} lg={4}>
+            <a href="https://www.youtube.com/channel/UC5phB7T3bEO57WzffPKCBNw" rel="noreferrer" target="_blank">Dr. Abla Hasan YouTube Channel</a>
+          </Col>
+          <Col className="mb-4 text-center" md={6} lg={4}>
+            <a href="https://www.youtube.com/channel/UCWUuSntbowhDJU9PdFA1CSQ" rel="noreferrer" target="_blank">UNL Arabic Program YouTube Channel</a>
+          </Col>
+          <Col className="mb-4 text-center" md={6} lg={4}>
+            <a href="https://modlang.unl.edu/dr-abla-hasan" rel="noreferrer" target="_blank">My UNL Faculty Page</a>
+          </Col>
+          <Col className="mb-4 text-center" md={6} lg={4}>
+            <a href="https://www.instagram.com/unlarabic/?hl=en" rel="noreferrer" target="_blank">UNL Arabic Instagram Page</a>
+          </Col>
+          <Col className="mb-4 text-center" md={6} lg={4}>
+            <a href="https://www.facebook.com/UNLArabic/" rel="noreferrer" target="_blank">UNL Arabic Facebook Page</a>
+          </Col>
+          <Col className="mb-4 text-center" md={6} lg={4}>
+            <DownloadCV btnType="form" />
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col className="text-center">
+            <small>Copyright &copy; {new Date().getFullYear()} Abla Hasan</small>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
