127646e62cd9823fb1ac6f03e6a5839ec343acb0
Ahmad <88450377+Ahmad-Milan@users.noreply.github.com>
2024-04-07 11:52:37 -0500

    alert class added

--- src/components/contact/TextError.js
+++ src/components/contact/TextError.js
@@ -2,7 +2,7 @@ import React from 'react'
 // Error Message Alert Component
 function TextError(props) {
   return (
-    <div className="alert-danger px-3 py-1 mt-1 rounded">
+    <div className="alert alert-danger px-3 py-1 mt-1 rounded">
       {props.children}
     </div>
   )
