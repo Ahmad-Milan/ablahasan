01d52b56570ee5d54180cfd0157c4bf581d2d9c0
Ahmad <88450377+Ahmad-Milan@users.noreply.github.com>
2024-06-09 19:57:46 -0500

    css class added

--- src/components/shared/DownloadCV.js
+++ src/components/shared/DownloadCV.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import Resume from '../../static/Abla-Hasan-CV-04-23.pdf'
-import { AiOutlineDownload } from 'react-icons/ai'
+import React from "react"
+import Resume from "../../static/AblaHasan_Resume_2024.pdf"
+import { AiOutlineDownload } from "react-icons/ai"
 
-function DownloadCV({color}) {
+function DownloadCV({ color }) {
   return (
-    <div>
+    <div className="resume-btn mb-2">
       <a href={Resume} className={`${color}-btn px-4 btn`} download>
         Download CV <AiOutlineDownload className="mb-1 ms-2 fs-4" />
       </a>
